c3760354fe5025116f3aebaa83aec82ec5125f26
藤田優作 <marozaemon1217@gmail.com>
2021-01-11 18:56:48 +0900

    first commit

---
+++ RamenInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RamenInfo
+//
+//  Created by 藤田優作 on 2021/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
